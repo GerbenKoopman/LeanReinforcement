64a4ba8421e2150e96afda6ec03c9d0dcb3a2b8c
Gerben Koopman <gerbennkoopman@gmail.com>
2025-07-14 22:53:06 +0200

    chore: fix typesetting

--- src/lean_rl/__init__.py
+++ src/lean_rl/__init__.py
@@ -6,7 +6,13 @@ on theorem proving tasks using LeanDojo.
 """
 
 from .environment import LeanEnvironment, StepResult
-from .agents import BaseAgent, RandomAgent, WeightedRandomAgent, MCTSAgent, HierarchicalTransformerAgent
+from .agents import (
+    BaseAgent,
+    RandomAgent,
+    WeightedRandomAgent,
+    MCTSAgent,
+    HierarchicalTransformerAgent,
+)
 
 __all__ = [
     "LeanEnvironment",
