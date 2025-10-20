1b72805063e364be3e53d0da9c0d9d90ec0b01f9
Gerben Koopman <gerbennkoopman@gmail.com>
2025-10-20 14:46:02 +0200

    refactor: reorganize DataLoader methods into a class

--- src/dataloader.py
+++ src/dataloader.py
@@ -5,35 +5,54 @@ Data loader for LeanDojo traced repositories and theorems.
 from typing import List, Tuple
 from lean_dojo import LeanGitRepo, TracedRepo, TracedTheorem, trace
 
-
-def trace_repo(
-    url: str = "https://github.com/leanprover-community/mathlib4",
-    commit: str = "29dcec074de168ac2bf835a77ef68bbe069194c5",
-):
-    """
-    Traces a Lean Repository using the LeanDojo library.
-    """
-
-    repo = LeanGitRepo(url, commit)
-
-    traced_repo = trace(repo)
-
-    return traced_repo
+from lean_dojo import Theorem
+from ReProver.common import Corpus, Pos
 
 
-def load_theorems(repo: TracedRepo) -> Tuple[List[TracedTheorem], List[TracedTheorem]]:
-    """
-    Loads theorems and seperates them into tactic proof theorems and statement theorems.
-    """
+class DataLoader:
+    def __init__(self, jsonl_path: str = "corpus.jsonl"):
+        self.corpus = Corpus(jsonl_path)
 
-    traced_theorems = repo.get_traced_theorems()
-    tactic_theorems = []
-    statement_theorems = []
+    def trace_repo(
+        self,
+        url: str = "https://github.com/leanprover-community/mathlib4",
+        commit: str = "29dcec074de168ac2bf835a77ef68bbe069194c5",
+    ):
+        """
+        Traces a Lean Repository using the LeanDojo library.
+        """
 
-    for theorem in traced_theorems:
-        if theorem.get_tactic_proof() is not None:
-            tactic_theorems.append(theorem)
-        else:
-            statement_theorems.append(theorem)
+        repo = LeanGitRepo(url, commit)
 
-    return tactic_theorems, statement_theorems
+        traced_repo = trace(repo)
+
+        return traced_repo
+
+    def load_theorems(
+        self,
+        repo: TracedRepo,
+    ) -> Tuple[List[TracedTheorem], List[TracedTheorem]]:
+        """
+        Loads theorems and seperates them into tactic proof theorems and statement theorems.
+        """
+
+        traced_theorems = repo.get_traced_theorems()
+        tactic_theorems = []
+        statement_theorems = []
+
+        for theorem in traced_theorems:
+            if theorem.get_tactic_proof() is not None:
+                tactic_theorems.append(theorem)
+            else:
+                statement_theorems.append(theorem)
+
+        return tactic_theorems, statement_theorems
+
+    def get_premises(self, theorem: Theorem, theorem_pos: Pos) -> List[str]:
+        """Retrieve all accessible premises given a theorem."""
+        return [
+            str(p)
+            for p in self.corpus.get_accessible_premises(
+                str(theorem.file_path), theorem_pos
+            )
+        ]
