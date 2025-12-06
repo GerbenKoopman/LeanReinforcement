5096608a3dec87c916ab9dcae7a3d3708f442496
Gerben Koopman <gerbennkoopman@gmail.com>
2025-12-06 14:00:44 +0100

    chore: remove files

--- view_json.js
+++
@@ -1,4 +0,0 @@
-const data = require('./leandojo_benchmark_4/novel_premises/train.json');
-console.log('JSON data loaded. You can now inspect the "data" variable in the debug console.');
-// Add a breakpoint on the line below to inspect the data
-debugger; 
