1286231245e7b3b3154e4bd1086eb832d9608093
Gerben Koopman <gerbennkoopman@gmail.com>
2026-01-04 08:57:04 +0100

    chore: update pipeline script

--- job_files/run_pipeline.sh
+++ job_files/run_pipeline.sh
@@ -5,11 +5,6 @@
 JOB_ID_1=$(sbatch --parsable environment.job)
 echo "Submitted Environment Job: $JOB_ID_1"
 
-# 2. Submit the indexing job with a dependency on Job 1
-# afterok means "Run this ONLY after the previous job finished with no errors"
-JOB_ID_2=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 index_corpus.job)
-echo "Submitted Index Job: $JOB_ID_2 (Dependency: $JOB_ID_1)"
-
-# 3. Submit the training job with a dependency on Job 2
-JOB_ID_3=$(sbatch --parsable --dependency=afterok:$JOB_ID_2 train.job)
-echo "Submitted Train Job: $JOB_ID_3 (Dependency: $JOB_ID_2)"
+# 2. Submit the training job with a dependency on Job 1
+JOB_ID_2=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 guided_rollout_training.job)
+echo "Submitted Train Job: $JOB_ID_2 (Dependency: $JOB_ID_1)"
