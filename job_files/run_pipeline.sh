f4e9b11431f67a8e848f0fc352ecdeb3345c4d51
Gerben Koopman <gerbennkoopman@gmail.com>
2026-03-19 08:44:09 +0100

    chore: update jobs

--- job_files/run_pipeline.sh
+++ job_files/run_pipeline.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# 1. Submit the environment setup job and capture its Job ID
-# --parsable makes sbatch return ONLY the job ID (e.g., 12345)
-# JOB_ID_1=$(sbatch --parsable environment.job)
-# echo "Submitted Environment Job: $JOB_ID_1"
+# JOB_ID_1=$(sbatch --parsable cython.job)
+# echo "Submitted Cython Job: $JOB_ID_1"
 
 # 2. Submit the training jobs with a dependency on Job 1
 
-JOB_ID_2=$(sbatch --parsable --dependency=afterok:20900517 train_mcts_euclidean.job)
-echo "Submitted Train Job: $JOB_ID_2 (Dependency: 20900517)"
+JOB_ID_2=$(sbatch --parsable --dependency=afterok:JOB_ID_1 train_mcts_euclidean.job)
+echo "Submitted Train Job: $JOB_ID_2 (Dependency: $JOB_ID_1)"
 
-JOB_ID_3=$(sbatch --parsable --dependency=afterok:20900517 train_mcts_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_3 (Dependency: 20900517)"
+JOB_ID_3=$(sbatch --parsable --dependency=afterok:JOB_ID_1 train_mcts_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_3 (Dependency: $JOB_ID_1)"
 
-JOB_ID_4=$(sbatch --parsable --dependency=afterok:20900517 train_ppo_euclidean.job)
-echo "Submitted Train Job: $JOB_ID_4 (Dependency: 20900517)"
+JOB_ID_4=$(sbatch --parsable --dependency=afterok:JOB_ID_1 train_ppo_euclidean.job)
+echo "Submitted Train Job: $JOB_ID_4 (Dependency: $JOB_ID_1)"
 
-JOB_ID_5=$(sbatch --parsable --dependency=afterok:20900517 train_ppo_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_5 (Dependency: 20900517)"
+JOB_ID_5=$(sbatch --parsable --dependency=afterok:JOB_ID_1 train_ppo_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_5 (Dependency: $JOB_ID_1)"
