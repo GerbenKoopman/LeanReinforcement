84df91a65773cda2eb878551bcbfd9407b122bde
Gerben Koopman <gerbennkoopman@gmail.com>
2026-04-03 14:28:24 +0200

    chore: update job

--- job_files/run_experiments.sh
+++ job_files/run_experiments.sh
@@ -3,23 +3,23 @@
 JOB_ID_1=$(sbatch --parsable train_mcts_euclidean.job)
 echo "Submitted Train Job: $JOB_ID_1"
 
-JOB_ID_2=$(sbatch --parsable --export=curvature=0.1 train_mcts_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_2"
+JOB_ID_2=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 --export=curvature=0.1 train_mcts_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_2, Starts if $JOB_ID_1 finishes succesfully"
 
-JOB_ID_3=$(sbatch --parsable --export=curvature=0.5 train_mcts_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_3"
+JOB_ID_3=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 --export=curvature=0.5 train_mcts_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_3, Starts if $JOB_ID_1 finishes succesfully"
 
-JOB_ID_4=$(sbatch --parsable --export=curvature=1.0 train_mcts_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_4"
+JOB_ID_4=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 --export=curvature=1.0 train_mcts_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_4, Starts if $JOB_ID_1 finishes succesfully"
 
-JOB_ID_5=$(sbatch --parsable train_ppo_euclidean.job)
-echo "Submitted Train Job: $JOB_ID_5"
+JOB_ID_5=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 train_ppo_euclidean.job)
+echo "Submitted Train Job: $JOB_ID_5, Starts if $JOB_ID_1 finishes succesfully"
 
-JOB_ID_6=$(sbatch --parsable --export=curvature=0.1 train_ppo_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_6"
+JOB_ID_6=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 --export=curvature=0.1 train_ppo_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_6, Starts if $JOB_ID_1 finishes succesfully"
 
-JOB_ID_7=$(sbatch --parsable --export=curvature=0.5 train_ppo_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_7"
+JOB_ID_7=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 --export=curvature=0.5 train_ppo_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_7, Starts if $JOB_ID_1 finishes succesfully"
 
-JOB_ID_8=$(sbatch --parsable --export=curvature=1.0 train_ppo_hyperbolic.job)
-echo "Submitted Train Job: $JOB_ID_8"
+JOB_ID_8=$(sbatch --parsable --dependency=afterok:$JOB_ID_1 --export=curvature=1.0 train_ppo_hyperbolic.job)
+echo "Submitted Train Job: $JOB_ID_8, Starts if $JOB_ID_1 finishes succesfully"
